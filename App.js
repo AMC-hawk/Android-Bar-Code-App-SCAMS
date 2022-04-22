a637870ea8ccae81bf9a18599efec1bb14bc1a05
AMC-hawk <63185874+AMC-hawk@users.noreply.github.com>
2022-04-22 21:01:08 +0530

    First Commit

---
+++ App.js
@@ -0,0 +1,132 @@
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import Realm from "realm";
+
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState("Not yet scanned");
+  const axios = require('axios');
+
+
+const TaskSchema = {
+  name: "Task",
+  properties: {
+    _id: "int",
+    name: "string",
+    status: "string?",
+  },
+  primaryKey: "_id",
+};
+
+const realm = async() => await Realm.open({
+  path: "myrealm",
+  schema: [TaskSchema],
+});
+
+// Add a couple of Tasks in a single, atomic transaction
+let task1, task2;
+realm.write(() => {
+  task1 = realm.create("Task", {
+    _id: 1,
+    name: "go grocery shopping",
+    status: "Open",
+  });
+
+  task2 = realm.create("Task", {
+    _id: 2,
+    name: "go exercise",
+    status: "Open",
+  });
+  console.log(`created two tasks: ${task1.name} & ${task2.name}`);
+});
+// use task1 and task2
+
+
+
+
+  const askForCameraPermission = () => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  };
+
+  // Request Camera Permission
+  useEffect(() => {
+    askForCameraPermission();
+  }, []);
+
+  // What happens when we scan the bar code
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setText(data);
+    const resp = axios.post("https://webhook.site/b2b179e4-58bf-4f58-8b4c-2547ab82916f", ["hello"]);
+    console.log(resp);
+    console.log("Type: " + type + "\nData: " + data);
+  };
+
+  // Check permissions and return the screens
+  if (hasPermission === null) {
+    return (
+      <View style={styles.container}>
+        <Text>Requesting for camera permission</Text>
+      </View>
+    );
+  }
+  if (hasPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ margin: 10 }}>No access to camera</Text>
+        <Button
+          title={"Allow Camera"}
+          onPress={() => askForCameraPermission()}
+        />
+      </View>
+    );
+  }
+
+  // Return the View
+  return (
+    <View style={styles.container}>
+      <View style={styles.barcodebox}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={{ height: 400, width: 400 }}
+        />
+      </View>
+      <Text style={styles.maintext}>{text}</Text>
+
+      {scanned && (
+        <Button
+          title={"Scan again?"}
+          onPress={() => setScanned(false)}
+          color="green"
+        />
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  maintext: {
+    fontSize: 16,
+    margin: 20,
+  },
+  barcodebox: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 300,
+    width: 300,
+    overflow: "hidden",
+    borderRadius: 30,
+    backgroundColor: "tomato",
+  },
+});
