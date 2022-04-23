9cf3c8d164199cdd16158502db95a700c0412e51
Azam Siddiqui <siddiquiazam966@gmail.com>
2022-04-23 17:37:00 +0530

    backend integration

--- App.js
+++ App.js
@@ -1,50 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
-import Realm from "realm";
+import { BASE_URL } from "@env";
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
   const [text, setText] = useState("Not yet scanned");
-  const axios = require('axios');
-
-
-const TaskSchema = {
-  name: "Task",
-  properties: {
-    _id: "int",
-    name: "string",
-    status: "string?",
-  },
-  primaryKey: "_id",
-};
-
-const realm = async() => await Realm.open({
-  path: "myrealm",
-  schema: [TaskSchema],
-});
-
-// Add a couple of Tasks in a single, atomic transaction
-let task1, task2;
-realm.write(() => {
-  task1 = realm.create("Task", {
-    _id: 1,
-    name: "go grocery shopping",
-    status: "Open",
-  });
-
-  task2 = realm.create("Task", {
-    _id: 2,
-    name: "go exercise",
-    status: "Open",
-  });
-  console.log(`created two tasks: ${task1.name} & ${task2.name}`);
-});
-// use task1 and task2
-
-
-
+  const axios = require("axios");
 
   const askForCameraPermission = () => {
     (async () => {
@@ -58,13 +21,36 @@ realm.write(() => {
     askForCameraPermission();
   }, []);
 
+  const scanLuggage = async (luggage_id) => {
+    console.log("Scanning luggage");
+    await axios
+      .post(
+        `https://da0c-112-79-165-230.ngrok.io/api/scanLuggage`,
+        {},
+        {
+          params: {
+            luggage_id: luggage_id,
+          },
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Accept: "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        setText(response.data.message);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      })
+      .finally(() => {});
+  };
+
   // What happens when we scan the bar code
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
-    setText(data);
-    const resp = axios.post("https://webhook.site/b2b179e4-58bf-4f58-8b4c-2547ab82916f", ["hello"]);
-    console.log(resp);
-    console.log("Type: " + type + "\nData: " + data);
+    setText("Scanning....");
+    scanLuggage(data);
   };
 
   // Check permissions and return the screens
